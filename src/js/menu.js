5b52690365e57adc34af46290c7d6c28791093bf
whiteroma <roma.white95@gmail.com>
2022-06-02 23:16:47 +0300

    overflow hidden

--- src/js/menu.js
+++ src/js/menu.js
@@ -10,5 +10,8 @@
         menuBtnRef.setAttribute("aria-expanded", !expanded);
   
         mobileMenuRef.classList.toggle("is-open");
+        document.body.classList.toggle('no-scroll');
     })
   })();
+
+  
