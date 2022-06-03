81a19e588641b48319559627aee35a4e8e6a5a1b
whiteroma <roma.white95@gmail.com>
2022-06-03 11:05:48 +0300

    v

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@
         menuBtnRef.setAttribute("aria-expanded", !expanded);
   
         mobileMenuRef.classList.toggle("is-open");
+        document.body.classList.toggle('no-scroll');
     })
 })();
 
